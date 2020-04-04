745880ebbbf1e30b547d69ecbbaf28f8c9471a1b
Aidar Habibullin <zoomme.kzn@gmail.com>
2020-04-04 05:49:06 +0300

    - Создание страницы Signup.

---
+++ src/components/pages/Signup/StyledSignup.jsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+import loginBackround from '../../../assets/images/login-background.png';
+
+export const StyledSignup = styled.div`
+    min-height: 100vh;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url('${loginBackround}');
+    background-size: cover;
+    background-position: center;
+    
+    @media (max-width: 800px) {
+        flex-direction: column;
+    }
+`;
+
+export const FormContainer = styled.div`
+    margin-left: 200px;
+    width: 500px;
+    padding: 60px 50px;
+    background-color: ${props => props.theme.white};
+
+    @media (max-width: 800px) {
+        margin-left: 0;
+        margin-top: 20px;
+    }
+`;
+
+export const Text = styled.div`
+    margin-bottom: 40px;
+`;
+
+export const Button = styled.button`
+    color: ${props => props.theme.secondary};
+    font-size: 16px;
+    font-weight: 400;
+    text-decoration: none;
+`;
+
+export const Label = styled.label`
+    margin-bottom: 8px;
+    display: block;
+    color: ${props => props.theme.gray};
+    font-size: 15px;
+    font-weight: 400;
+`;
+
+export const Form = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
