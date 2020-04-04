5c5dd2037e3c3c5536439c2346652a1e6ef1824f
Aidar Habibullin <zoomme.kzn@gmail.com>
2020-04-04 05:48:09 +0300

    - Создание страницы Login.

---
+++ src/components/pages/Login/Login.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+import Logo from '../../shared/Logo';
+import Heading from '../../shared/Heading';
+import Input from '../../shared/Input';
+import Submit from '../../shared/Submit';
+
+import { StyledLogin, Form, FormContainer, Text, Button, Label } from './StyledLogin';
+
+class Login extends Component {
+    handleSubmit = event => {
+        event.preventDefault();
+        this.props.setPage('map');
+    };
+
+    showSignupPage = () => this.props.setPage('signup');
+
+    render() {
+        return (
+            <StyledLogin>
+                <Logo />
+                <FormContainer>
+                    <Heading title="Войти" />
+                    <Text>
+                        Новый пользователь?&nbsp;
+                        <Button onClick={this.showSignupPage}>Зарегистрируйтесь</Button>
+                    </Text>
+                    <Form onSubmit={this.handleSubmit}>
+                        <Label htmlFor="username">Имя пользователя*</Label>
+                        <Input type="text" id="username" placeholder="Введите имя" />
+                        <Label htmlFor="password">Пароль*</Label>
+                        <Input type="password" id="password" placeholder="Введите пароль" />
+                        <Submit name="Войти" />
+                    </Form>
+                </FormContainer>
+            </StyledLogin>
+        );
+    }
+}
+
+export default Login;
